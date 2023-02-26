bac097c64bf0c9f1a48ed80fac3ebe8aca4fb1f4
Sezgi Altan <sezgialtan@Sezgis-MacBook-Air.local>
2023-02-26 23:02:14 +0300

    test(hero):: videos

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,24 +9,24 @@
                         class="makeItBlack">özgün ve kaliteli tasarımlar</span> oluşturarak alanında fark yaratmayı
                     amaçlayan,
                     <span class="makeItBlack">Denizli</span> merkezli bir tasarım stüdyosudur.</span>
-                <!-- <img src="/hero/hero-gif-1.gif" class="d-none d-md-block mt-0 mt-md-5 " alt="" style="max-width: 100%;"> -->
-                <video class="rounded" src="/hero/hero-video-1.mov" autoplay muted loop />
+                <!-- <img src="/hero/hero-1.jpg" class="d-none d-md-block mt-0 mt-md-5 " alt="" style="max-width: 100%;"> -->
+                <video class="rounded d-none d-md-block mt-0 mt-md-5" :src="'/hero/' + videos[0]" autoplay muted loop />
             </div>
             <div class="col-12 col-md-4 px-4 d-flex flex-column justify-content-between mt-4 mt-md-0 text-justify">
                 <span class="mb-0 mb-md-5 text-artrodite-gray animate__animated animate__fadeInUp">
                     Çalışmalarımız sizin ihtiyaçlarınıza özel olarak tasarlanır. <span class="makeItBlack"> Önceden
                         hazırlanmış taslaklar
                         kullanılmaz.</span></span>
-                <!-- <img src="/hero/hero-gif-2.gif" class="d-none d-md-block mt-0 mt-md-5" alt="" style="max-width: 100%;"> -->
-                <video class="rounded" src="/hero/hero-video-2.mov" autoplay muted loop />
+                <!-- <img src="/hero/hero-2.jpg" class="d-none d-md-block mt-0 mt-md-5" alt="" style="max-width: 100%;"> -->
+                <video class="rounded d-none d-md-block mt-0 mt-md-5" :src="'/hero/' + videos[1]" autoplay muted loop />
 
             </div>
             <div class="col-12 col-md-4 px-4 d-flex flex-column justify-content-between my-4 my-md-0">
                 <router-link class="mb-0 mb-md-5 makeItBlack animate__animated animate__fadeInUp" to="/studio"> <span
                         class="hover-underline-animation"> Detaylı Bilgi <i class="fa-solid fa-chevron-right" />
                     </span></router-link>
-                <!-- <img src="/hero/hero-gif-3.gif" class="d-none d-md-block mt-0 mt-md-5" alt="" style="max-width: 100%;"> -->
-                <video class="rounded" src="/hero/hero-video-3.mov" autoplay muted loop />
+                <!-- <img src="/hero/hero-3.jpg" class="d-none d-md-block mt-0 mt-md-5" alt="" style="max-width: 100%;"> -->
+                <video class="rounded d-none d-md-block mt-0 mt-md-5" :src="'/hero/' + videos[2]" autoplay muted loop />
 
             </div>
             <img src="../assets/hero/hero-mobile.jpg" class="d-block d-md-none" alt="">
@@ -34,7 +34,32 @@
     </div>
 </template>
 
-<script setup>
+<script>
+export default {
+    data(){
+        return {
+            videos: ['hero-video-1.mov', 'hero-video-2.mov', 'hero-video-3.mov'],
+            videosLoaded: false
+        }
+    },
+    mounted() {
+        const promises = [];
+        for (let i = 0; i < this.videos.length; i++) {
+            const promise = new Promise((resolve) => {
+                const video = document.createElement("video");
+                video.addEventListener("canplaythrough", () => {
+                    resolve();
+                });
+                video.src = this.videos[i].url;
+                video.load();
+            });
+            promises.push(promise);
+        }
+        Promise.all(promises).then(() => {
+            this.videosLoaded = true;
+        });
+    }
+}
 
 </script>
 
