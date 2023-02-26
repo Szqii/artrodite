58d41bfd980fc719e9632329027b01d5db40c22b
Sezgi Altan <sezgialtan@Sezgis-MacBook-Air.local>
2023-02-26 21:26:43 +0300

    update():: artrodite

--- src/views/StudioView.vue
+++ src/views/StudioView.vue
@@ -1,51 +1,62 @@
 <template>
-    <div class="row px-100">
-        <div class="col-12 row" style="height: 100vh;">
-            <div class="col-3 position-relative">
+    <div class="row px-32px px-lg-100 mx-0">
+        <div class="col-12 row mx-auto mx-lg-0 h-lg-screen">
+            <div class="col-12 col-lg-3 position-relative px-0 mb-4 mb-lg-0">
                 <div class="sticky-top title-text animate__animated animate__fadeInUp animate__delay_100ms">Biz</div>
             </div>
-            <div class="col-3 d-flex flex-column gap-130px px-4 text-justify text-artrodite-gray">
-                <span class="animate__animated animate__fadeInUp"> Günümüzde, markaların kişilikleri, algımızda çok büyük bir önem taşır. <span class="makeItBlack"> Kişilikli bir marka, </span> sizi rakiplerinizden ayırır ve öne çıkmanızı sağlar.</span>
-                <span class="animate__animated animate__fadeInUp animate__delay_200ms"> Projeler daima sizin ihtiyaçlarınıza özel olarak tasarlanır. <span class="makeItBlack"> Hazır taslaklar kullanılmaz. </span> </span>
+            <div
+                class="col-12 col-lg-3 d-flex flex-column gap-4 gap-lg-130px px-0 px-lg-4 text-justify text-artrodite-gray">
+                <span class="animate__animated animate__fadeInUp"> Günümüzde, markaların kişilikleri, algımızda çok büyük
+                    bir önem taşır. <span class="makeItBlack"> Kişilikli bir marka, </span> sizi rakiplerinizden ayırır ve
+                    öne çıkmanızı sağlar.</span>
+                <span class="animate__animated animate__fadeInUp animate__delay_200ms"> Projeler daima sizin ihtiyaçlarınıza
+                    özel olarak tasarlanır. <span class="makeItBlack"> Hazır taslaklar kullanılmaz. </span> </span>
             </div>
-            <div class="col-3 text-justify px-4 text-artrodite-gray ">
+            <div class="col-12 col-lg-3 text-justify px-0 px-lg-4 text-artrodite-gray ">
                 <div class="animate__animated animate__fadeInUp">
                     <span class="makeItBlack">Artrodite’nin</span> ilk amacı markanız için <br>
-                    <span class="makeItBlack">göz alıcı bir kişilik</span> oluşturarak, potansiyel müşterilerinizi arttırmayı ve işletmenizi hak ettiği başarıya ulaştırmaktır.
+                    <span class="makeItBlack">göz alıcı bir kişilik</span> oluşturarak, potansiyel müşterilerinizi
+                    arttırmayı ve işletmenizi hak ettiği başarıya ulaştırmaktır.
                 </div>
             </div>
-            <div class="col-3">
-                <img src="../assets/studio/studio-1.png" alt="">
+            <div class="col-12 col-lg-3 px-0 mt-4 mt-lg-0">
+                <img src="../assets/studio/studio-1.png" alt="" class="w-100">
             </div>
         </div>
-        <div class="col-12 row" style="height: 100vh;">
-            <div class="col-3 position-relative">
+
+        <div class="col-12 row h-lg-screen mt-5 mx-auto mx-lg-0 text-center text-lg-start">
+            <div class="col-12 col-lg-3 position-relative my-5 my-lg-0">
                 <div class="sticky-top title-text animate__animated animate__fadeInUp">Metadoloji</div>
             </div>
 
-            <div class="col-3">
+            <div class="col-12 col-lg-3">
                 <ul class="list-unstyled d-flex flex-column gap-4">
                     <li class="animate__animated animate__fadeInUp animate__delay_100ms">Dinle</li>
-                    <li class="animate__animated animate__fadeInUp animate__delay_200ms"><i class="fa fa-chevron-down"></i></li>
+                    <li class="animate__animated animate__fadeInUp animate__delay_200ms"><i class="fa fa-chevron-down"></i>
+                    </li>
                     <li class="animate__animated animate__fadeInUp animate__delay_300ms">Araştır</li>
-                    <li class="animate__animated animate__fadeInUp animate__delay_400ms"><i class="fa fa-chevron-down"></i></li>
+                    <li class="animate__animated animate__fadeInUp animate__delay_400ms"><i class="fa fa-chevron-down"></i>
+                    </li>
                     <li class="animate__animated animate__fadeInUp animate__delay_500ms">Tasarla</li>
-                    <li class="animate__animated animate__fadeInUp animate__delay_600ms"><i class="fa fa-chevron-down"></i></li>
+                    <li class="animate__animated animate__fadeInUp animate__delay_600ms"><i class="fa fa-chevron-down"></i>
+                    </li>
                     <li class="animate__animated animate__fadeInUp animate__delay_700ms">Test et</li>
-                    <li class="animate__animated animate__fadeInUp animate__delay_800ms"><i class="fa fa-chevron-down"></i></li>
+                    <li class="animate__animated animate__fadeInUp animate__delay_800ms"><i class="fa fa-chevron-down"></i>
+                    </li>
                     <li class="animate__animated animate__fadeInUp animate__delay_900ms">Tekrarla</li>
                 </ul>
             </div>
-            <div class="col-3" />
-            <div class="col-3">
-                <img src="../assets/studio/studio-2.png" alt="">
+            <div class="col-12 col-lg-3" />
+            <div class="col-12 col-lg-3 px-0 mt-4 mt-lg-0">
+                <img src="../assets/studio/studio-2.png" alt="" class="w-100">
             </div>
         </div>
-        <div class="col-12 row" style="height: 100vh;">
-            <div class="col-3 position-relative">
+
+        <div class="col-12 row h-lg-screen mt-5 mx-auto mx-lg-0">
+            <div class="col-12 col-lg-3 position-relativev my-5 my-lg-0">
                 <div class="sticky-top title-text animate__animated animate__fadeInUp">Çalışma Alanlarımız</div>
             </div>
-            <div class="col-3">
+            <div class="col-12 col-lg-3">
                 <ul class="list-unstyled d-flex flex-column gap-4">
                     <li class="animate__animated animate__fadeInUp animate__delay_100ms"> Kurumsal Kimlik Tasarımı </li>
                     <li class="animate__animated animate__fadeInUp animate__delay_200ms"> UI - UX Tasarımı </li>
@@ -55,13 +66,13 @@
                     <li class="animate__animated animate__fadeInUp animate__delay_600ms"> Reklamcılık </li>
                     <li class="animate__animated animate__fadeInUp animate__delay_700ms"> Katalog tasarımı </li>
                     <li class="animate__animated animate__fadeInUp animate__delay_800ms"> Ambalaj Tasarmı </li>
-                    <li class="animate__animated animate__fadeInUp animate__delay_900ms"> Kart visit, defter, takvim vb. </li>
+                    <li class="animate__animated animate__fadeInUp animate__delay_900ms"> Kart visit, defter, takvim vb.
+                    </li>
                 </ul>
             </div>
-            <div class="col-3" />
-
-            <div class="col-3">
-                <img src="../assets/studio/studio-3.png" alt="">
+            <div class="col-12 col-lg-3" />
+            <div class="col-12 col-lg-3 px-0 mt-4 mt-lg-0">
+                <img src="../assets/studio/studio-3.png" alt="" class="w-100">
             </div>
         </div>
     </div>
@@ -71,8 +82,6 @@
 
 </script>
 
-<style lang="scss" scoped>
-.sticky-top{
+<style lang="scss" scoped>.sticky-top {
     top: 20px;
-}
-</style>
+}</style>
