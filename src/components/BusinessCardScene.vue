6daf845534312432af4ace7a680d496bde025480
Szqii <szqii.altn@gmail.com>
2023-03-24 20:06:10 +0300

    update(business-card):: prevent zoom in

--- src/components/BusinessCardScene.vue
+++ src/components/BusinessCardScene.vue
@@ -1,16 +1,16 @@
 <template>
     <!-- eslint-disable  -->
     <Suspense>
-        <TresCanvas shadows alpha :output-encoding="sRGBEncoding">
-            <OrbitControls />
+        <TresCanvas preset="realistic">
+            <OrbitControls id="orbitControls" :minPolarAngle="Math.PI / 2" :maxPolarAngle="Math.PI / 2" :enableZoom="false"/>
             <!-- <TresPerspectiveCamera :position="[0, 0, 10]" :fov="75" :near="1" :far="100" /> -->
-            <TresPerspectiveCamera :position="[5, 0, -10]" :fov="75" :aspect="1" :near="0.1" :far="1000" />
+            <TresPerspectiveCamera :position="cameraPosition" :fov="75" :aspect="1" :near="0.1" :far="1000" />
             <TresScene :fog="'#82DBC5'">
                 <!-- <TresMesh /> -->
                 <GLTFModel ref="cardRef" path="/models/business-card/card.gltf" draco />
-                <TresAmbientLight :intensity="2" />
-                <TresDirectionalLight :position="[0, 0, 20]" :intensity="2" cast-shadow />
-                <TresDirectionalLight :position="[0, 0, -20]" :intensity="2" cast-shadow />
+                <!-- <TresAmbientLight :intensity="2" /> -->
+                <TresDirectionalLight :position="[0, 0, 20]" :intensity=".6" cast-shadow />
+                <TresDirectionalLight :position="[0, 0, -20]" :intensity=".6" cast-shadow />
             </TresScene>
         </TresCanvas>
     </Suspense>
@@ -18,13 +18,13 @@
 
 
 <script setup>
-import {  shallowRef } from 'vue';
+import { shallowRef } from 'vue';
 import { OrbitControls, GLTFModel } from '@tresjs/cientos';
 // import { useRenderLoop } from '@tresjs/core';
 
 
 const cardRef = shallowRef();
-
+const cameraPosition = [0, 0, -13];
 // const { onLoop } = useRenderLoop()
 
 // onLoop(({ delta }) => {
@@ -35,9 +35,12 @@ const cardRef = shallowRef();
 // })
 
 
+if (window.innerWidth > 991) {
+    cameraPosition[2] = -10;
+}
+
+
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
