6ece6fefd84d4d76cf9bc16f1678616c1aa8ed0e
Szqii <szqii.altn@gmail.com>
2023-04-06 12:12:47 +0300

    fix():: typo

--- src/main.js
+++ src/main.js
@@ -13,10 +13,10 @@ export const app = createApp(App)
 app.use(router)
 app.use(VueLazyload)
 app.use(Tres)
-app.mount('#app')
 app.use(VueAnalytics, {
     id: 'G-4KVXM081NZ',
     router
 })
+app.mount('#app')
 
 // createApp(App).use(router).use(VueLazyload).use(Tres).mount('#app')
