523c429ad702e41ddf7792096f4ade97d731a92b
Szqii <szqii.altn@gmail.com>
2023-04-06 12:12:09 +0300

    update():: vue analytics with google analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import 'animate.css';
 import VueLazyload from 'vue-lazyload'
 import Tres from '@tresjs/core'
+import VueAnalytics from 'vue-analytics';
+
 
 
 export const app = createApp(App)
@@ -12,5 +14,9 @@ app.use(router)
 app.use(VueLazyload)
 app.use(Tres)
 app.mount('#app')
+app.use(VueAnalytics, {
+    id: 'G-4KVXM081NZ',
+    router
+})
 
 // createApp(App).use(router).use(VueLazyload).use(Tres).mount('#app')
